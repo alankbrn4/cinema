c66195dfa3492c4ba0708fcd18fc68143df11bde
ALAN47 <alankbrn4@gmail.com>
2023-11-27 17:01:20 -0600

    revision5

--- src/main/java/com/example/demo/security/UserRepository.java
+++ src/main/java/com/example/demo/security/UserRepository.java
@@ -5,8 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> e3fd7648bbe5744615b64a20bb809a37f0a645fc
